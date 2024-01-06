e304df38677a2f4ace250f20e37339e555e990e4
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-06 08:54:06 -0800

    chore: workspaces

--- terraform/tfe-bootstrap/workspace_specifications.tf
+++ terraform/tfe-bootstrap/workspace_specifications.tf
@@ -1,15 +1,23 @@
 locals {
   workspaces = [
+    # Usually I'd split up workspaces to make planning/runs faster with less
+    # resources managed in a single mega-workspace, however for this demo
+    # just gonna make one workspace to save on resource count (outputs, data referencing)
+    # due to free tier
+    # {
+    #   path = "terraform/azure/networking"
+    #   envs = ["dev"]
+    # },
+    # {
+    #   path = "terraform/azure/databases"
+    #   envs = ["dev"]
+    # },
+    # {
+    #   path = "terraform/azure/identities"
+    #   envs = ["dev"]
+    # }
     {
-      path = "terraform/azure/networking"
-      envs = ["dev"]
-    },
-    {
-      path = "terraform/azure/databases"
-      envs = ["dev"]
-    },
-    {
-      path = "terraform/azure/identities"
+      path = "terraform/azure"
       envs = ["dev"]
     }
   ]
