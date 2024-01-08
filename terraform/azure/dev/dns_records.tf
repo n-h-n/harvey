b2eb5b513cdc9bd5a161e30be31a77b2b939d78b
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-08 10:00:08 -0800

    chore: networking debug

--- terraform/azure/dev/dns_records.tf
+++ terraform/azure/dev/dns_records.tf
@@ -13,11 +13,3 @@ resource "azurerm_dns_a_record" "dataroom-frontend" {
   ttl                 = 180
   target_resource_id  = data.azurerm_public_ip.appgw_public_ip.id
 }
-
-resource "azurerm_dns_a_record" "dataroom-backend" {
-  name                = "dataroom-backend"
-  zone_name           = azurerm_dns_zone.dns["app-eastus"].name
-  resource_group_name = azurerm_resource_group.rg["app-eastus"].name
-  ttl                 = 180
-  target_resource_id  = data.azurerm_public_ip.appgw_public_ip.id
-}
