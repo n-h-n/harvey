5b9eecde6adaaf99d1b93ccf6889d96d81841d51
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-08 14:31:00 -0800

    debug: testing something

--- services/dataroom-frontend/src/services/api.tsx
+++ services/dataroom-frontend/src/services/api.tsx
@@ -2,7 +2,7 @@
 import axios, {AxiosInstance} from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://dataroom-backend.dataroom.svc.cluster.local:8000',  headers: {
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://testingsomething:8000',  headers: {
     'Content-Type': 'application/json',
     // Add any additional headers if needed
   },
