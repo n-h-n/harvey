7fdef5442e0227a653d96e1f4073b16a3b3281b5
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-08 14:51:18 -0800

    debug: change pathType to Exact

--- services/dataroom-frontend/src/services/api.tsx
+++ services/dataroom-frontend/src/services/api.tsx
@@ -2,7 +2,7 @@
 import axios, {AxiosInstance} from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://testingsomething:8000',  headers: {
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',  headers: {
     'Content-Type': 'application/json',
     // Add any additional headers if needed
   },
