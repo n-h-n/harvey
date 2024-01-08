e0bc30c553d7bf335a5273313e05eb4da52a1221
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-08 08:41:46 -0800

    chore: testing stuff

--- terraform/azure/dev/network.tf
+++ terraform/azure/dev/network.tf
@@ -79,6 +79,19 @@ resource "azurerm_network_security_group" "rg_sg" {
       destination_address_prefix = "*"
     }
   }
+
+  // Debugging
+  security_rule {
+    name                       = "all"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_security_group" "alb_sg" {
