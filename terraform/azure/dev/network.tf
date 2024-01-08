57223e46aa0ea3e6967c7b72acae19fbe7eac031
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-07 17:28:11 -0800

    chore: alb sg rules

--- terraform/azure/dev/network.tf
+++ terraform/azure/dev/network.tf
@@ -100,13 +100,25 @@ resource "azurerm_network_security_group" "alb_sg" {
   }
 
   security_rule {
-    name                       = "web"
+    name                       = "web-https"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "web-http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
