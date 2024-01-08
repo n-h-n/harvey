e67f09547521ac69315c6796cb2fa1dfb74c3e78
Nghia Nguyen <nhn@alumni.stanford.edu>
2024-01-07 22:42:27 -0800

    fix: ACR naming

--- terraform/azure/dev/acr.tf
+++ terraform/azure/dev/acr.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "acr" {
   for_each = azurerm_resource_group.rg
 
-  name                = "acr${each.value.location}" // alphanumeric only
+  name                = "greywindacr${each.value.location}" // alphanumeric only, globally unique
   resource_group_name = each.key
   location            = each.value.location
   sku                 = "Basic"
--- terraform/azure/dev/keyvault.tf
+++ terraform/azure/dev/keyvault.tf
@@ -7,6 +7,7 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
+  purge_protection_enabled   = false
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
